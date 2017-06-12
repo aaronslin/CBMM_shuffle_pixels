d62d678dbef160b9b4a1ef871324f3a4d1dc4c73
Aaron Lin <lin.aaron1@gmail.com>
2017-06-12 13:56:52 -0400

    filled in _coord_map() for mapping (x,y) to its new coordinate

--- frame_shuffle.py
+++ frame_shuffle.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+
+# Generator functions
 def pow2_dimensions(image):
 	# Takes an input image (e.g. MNIST: 28 x 28)
 	# Returns image with power-of-2 dimensions (32 x 32)
@@ -9,16 +11,31 @@ def pow2_dimensions(image):
 def generate_shuffle_map(logDim):
 	pass
 
+
+
+# Shuffle functions 
 def _check_shuffle_map(map, logDim):
 	# Given map, check that dimensions are (2^logDim) x (2^logDim)
 	if map is None:
 		return False
 	pass
 
-def _shuffle_out(image, logDim, logPanes, map):
+def _coord_map(paneSize, x,y, outMap, inMap):
+	xq, xr = (x // paneSize, x % paneSize)
+	yq, yr = (y // paneSize, y % paneSize)
+
+	xq, yq = outMap[xq][yq]
+	xr, yr = inMap[xr][yr]
+
+	return (xq * paneSize + xr, yq * paneSize + yr)
+
+def _shuffle_out(image, logDim, logPanes, outMap):
+
+
+	# map a tuple in (range(32), range(32)) -> 
 	pass
 
-def _shuffle_in(image, logDim, logPanes, map):
+def _shuffle_in(image, logDim, logPanes, inMap):
 	pass
 
 def shuffle(image, logDim, logPanes, inShuffleMap=None, outShuffleMap=None):
@@ -32,3 +49,5 @@ def shuffle(image, logDim, logPanes, inShuffleMap=None, outShuffleMap=None):
 		image = _shuffle_in(image, logDim, logPanes, inShuffleMap)
 
 	return image 
+
+
